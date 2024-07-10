f6c2fbd4ee0a11f828605b8393f4a7c6cb86bf13
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2024-07-10 13:02:16 +0300

    update faucet

--- src/apis/faucet.ts
+++ src/apis/faucet.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Address } from 'viem';
 
-const API_URL = 'https://pyrqgkqq4j.execute-api.us-east-1.amazonaws.com/faucet'; // fast CORS fix
+const API_URL = 'https://pyrqgkqq4j.execute-api.us-east-1.amazonaws.com/faucet';
 
 const faucetAxiosApi = axios.create({
   baseURL: API_URL,
@@ -10,11 +10,8 @@ const faucetAxiosApi = axios.create({
 export async function addBalance(address: Address, amount: bigint) {
   if (process.env.NEXT_PUBLIC_GIT_BRANCH === 'main')
     throw new Error('No faucet available for mainnet!');
-  await faucetAxiosApi.post(
-    `/${process.env.NEXT_PUBLIC_TENDERLY_RPC_POSTFIX}`,
-    {
-      address,
-      amount: amount.toString(),
-    },
-  );
+  await faucetAxiosApi.post('/', {
+    address,
+    amount: amount.toString(),
+  });
 }
